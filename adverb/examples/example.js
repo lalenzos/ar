91f1d909d07bf2726b12e76fb2b655c47bdba5f9
Laurin Stricker <laurin.stricker@compunet-it.com>
2021-10-25 17:30:14 +0200

    Little changes.

--- adverb/examples/example.js
+++ adverb/examples/example.js
@@ -4,12 +4,11 @@ testVariable = "";
 const anotherVariableThatHasAnExtremelyLongVariableNameForTestingOfEdgeCasesSinceWeAlsoLikeToConsiderLongVariableNames2 = "Ciao";
 function test123(a, b, ...c) {
     console.log(a, b, ...c);
-};
-
+}
+;
 const test456 = (x) => {
     console.log(x);
 };
-
 test123('Foo', 'bar', 1, 2, 3, 4);
 console.log('a', 'b', 'c', 'd', 'e');
 console.log(anotherVariableThatHasAnExtremelyLongVariableNameForTestingOfEdgeCasesSinceWeAlsoLikeToConsiderLongVariableNames2);
